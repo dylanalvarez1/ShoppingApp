fa1d81a4c4377791c8fe188c859af9a901b1d2d9
dylanalvarez1 <dylanalvarez1@ufl.edu>
2018-11-26 19:45:49 -0500

    try to allow cross origin in express

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+app.use(allowCrossDomain);
 app.use(express.static(__dirname + '/'));
 
 let port = process.env.PORT || 3000;
